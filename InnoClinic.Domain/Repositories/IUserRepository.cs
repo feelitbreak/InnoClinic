9f6df74ca49a8113d68d63d9f6d181ef38d88213
feelitbreak <kendyslesha@gmail.com>
2023-08-10 13:34:01 +0300

    added IUserRepository interface

---
+++ InnoClinic.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using InnoClinic.Domain.Entities;
+
+namespace InnoClinic.Domain.Repositories
+{
+    internal interface IUserRepository
+    {
+        public List<User> GetUserDetails();
+
+        public User GetUserDetails(int id);
+
+        public void AddUser(User user);
+
+        public bool CheckUser(int id);
+    }
+}
