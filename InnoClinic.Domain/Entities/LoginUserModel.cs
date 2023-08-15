794b3deeaee4f79f5fd3e2f007649a622398e210
feelitbreak <kendyslesha@gmail.com>
2023-08-15 22:54:08 +0300

    login and register user models

---
+++ InnoClinic.Domain/Entities/LoginUserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Domain.Entities
+{
+    public class LoginUserModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6), MaxLength(15)]
+        public string Password { get; set; }
+    }
+}
---
+++ InnoClinic.Domain/Entities/RegisterUserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Domain.Entities
+{
+    public class RegisterUserModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6), MaxLength(15)]
+        public string Password { get; set; }
+
+        [Required]
+        [MinLength(6), MaxLength(15)]
+        public string ReenteredPassword { get; set; }
+    }
+}
