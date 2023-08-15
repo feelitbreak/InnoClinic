3c24dd645fb30232c2017cf275ee1ff685bbc6bc
feelitbreak <kendyslesha@gmail.com>
2023-08-15 22:57:51 +0300

    added mapping profiles class

---
+++ InnoClinic.Services/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InnoClinic.Domain.Entities;
+
+namespace InnoClinic.Services
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<RegisterUserModel, User>();
+            CreateMap<LoginUserModel, User>();
+        }
+    }
+}
