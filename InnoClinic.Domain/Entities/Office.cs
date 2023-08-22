4a7292ac58a1eba3a5d87e02eff7257cc0229d06
feelitbreak <kendyslesha@gmail.com>
2023-08-22 14:32:56 +0300

    added office entity

---
+++ InnoClinic.Domain/Entities/Office.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InnoClinic.Domain.Enums;
+
+namespace InnoClinic.Domain.Entities
+{
+    public class Office
+    {
+        public int Id { get; set; }
+
+        public byte[]? Photo { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        [Column("House number")]
+        public string HouseNumber { get; set; }
+
+        [Column("Office number")]
+        public string OfficeNumber { get; set; }
+
+        [Column("Registry phone number")]
+        public int RegistryPhoneNumber { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
