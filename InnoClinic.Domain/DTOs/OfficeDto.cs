45a702407b9a000a1a012cd0e5c5b9fae5004d00
feelitbreak <kendyslesha@gmail.com>
2023-08-22 14:36:11 +0300

    added office dto

---
+++ InnoClinic.Domain/DTOs/OfficeDto.cs
@@ -0,0 +1,27 @@
+﻿using InnoClinic.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Domain.DTOs
+{
+    public class OfficeDto
+    {
+        public byte[]? Photo { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string HouseNumber { get; set; }
+
+        public string OfficeNumber { get; set; }
+
+        public int RegistryPhoneNumber { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
