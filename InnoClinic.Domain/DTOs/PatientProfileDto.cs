3fdb74c586d8d774138d3007fabdc25d265e7ee1
feelitbreak <kendyslesha@gmail.com>
2023-09-07 12:57:00 +0300

    added patient profile info

---
+++ InnoClinic.Domain/DTOs/PatientProfileDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Domain.DTOs
+{
+    public class PatientProfileDto
+    {
+        public byte[]? Photo { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public long PhoneNumber { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
--- InnoClinic.Domain/Entities/User.cs
+++ InnoClinic.Domain/Entities/User.cs
@@ -13,6 +13,22 @@ namespace InnoClinic.Domain.Entities
 
         public Role Role { get; set; }
 
+        public bool IsEmailVerified { get; set; }
+
+        public byte[]? Photo { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public long PhoneNumber { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public bool? IsLinkedToAccount { get; set; }
+
         public int? OfficeId { get; set; }
 
         [JsonIgnore]
